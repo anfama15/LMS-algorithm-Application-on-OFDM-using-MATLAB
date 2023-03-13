fc5452ab5406e90d6108fdb2a76647ef31625954
anfama15 <63739458+anfama15@users.noreply.github.com>
2023-03-13 23:53:17 +0500

    Update ofdm_v4.m

--- ofdm_v4.m
+++ ofdm_v4.m
@@ -1,75 +1,84 @@
+% Clear the workspace and figure
+clear; clf;
 
-clear;clf;
-L=160000; % Total data symbols in experiment is 
-Lfr=L/32; % number of data frames
-% Generating random signal data for polar signaling
-s_data=4*round(rand(L,1))+2*round(rand( L,1))-3+...
-    +1i*(4*round(rand(L,1))+2*round(rand(L,1))-3); %16QAM Modulation
-abc=s_data;
-channel=[0.3 -0.5 0 1 .2 -0.3]; % channel in t-domain
-hf=fft(channel,32); % find the channel in f-domain
-p_data=reshape(s_data,32,Lfr); % S/P conversion
-first_data=inv(diag(hf))*p_data;
-p_td=ifft(p_data); % IDFT to convert to t-domain
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%cyclic prefix%%%%%%%%%%%%%%%%%%%%%%%%%
-p_cyc=[p_td(end-4:end,:);p_td]; % add cyclic prefix
-s_cyc=reshape(p_cyc,37*Lfr,1); % P/S conversion
-Psig=10/32; % average channel input power
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-chsout=filter(channel,1,s_cyc); % generate channel output signal
-clear p_td p_cyc s_data s_cyc; % release some memory
-noiseq=(randn(37*Lfr,1)+1i*randn(37*Lfr,1));
-SEReq=[];
-for ii=1:31,
-SNR(ii)=ii-1; % SNR in dB
-% Asig=sqrt(Psig*10^(-SNR(ii)/10))*norm(channel);
-% y_t=chsout+Asig*noiseq; % Add noise
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%I/Q Imbalance%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-alpha=chsout*0.3;
-y_conj=conj(chsout);
-beta=y_conj*0.7;
-y_bar=alpha+beta;
-%%
-%Noise
-n_para=reshape(y_bar,37,Lfr); % S/P conversion
-n_disc=n_para(6:37,:); % discard tails
-nhat_para=fft(n_disc); % FFT back to f-domain
-n_data=inv(diag(hf))*nhat_para; % f-domain equalizing
+% Define parameters
+L = 160000; % Total data symbols in experiment
+Lfr = L/32; % Number of data frames
+channel = [0.3, -0.5, 0, 1, 0.2, -0.3]; % Channel in time-domain
 
-%%
-%LMS
-%%
-%dereiving an algo to calculate convergence factor.
-mat=corrcoef(y_bar);
-xn=y_bar;
-dn=chsout;
-M=32;
-auto=xcorr(xn);
-%eigen=eig(auto);
-%mu=max(eigen);
-%Px=2/(32*auto);
-%mu=max(Px);
-%%
+% Generate random signal data for polar signaling
+s_data = 4*round(rand(L, 1)) + 2*round(rand(L, 1)) - 3 ...
+        + 1i*(4*round(rand(L, 1)) + 2*round(rand(L, 1)) - 3); % 16QAM modulation
 
-mu=0.00018; % mu convergence factor (step size) 
-itr = length(xn); 
-en = zeros(itr,1);     % error sequence, en(k) represents the error between the expected output and the actual input at the k-th iteration        
-W  = zeros(M,itr);     % Each row represents a weighting parameter, each column represents - iterations, and the initial value is 0
-% iterative calculation        
-for k = M:itr          % k-th iteration        
-    x = xn(k:-1:k-M+1); % input of filter M taps       
-    y = W(:,k-1).' * x; % output of the filter        
-    en(k) = dn(k) - y ; % error at iteration k       
-    % Iterative formula for filter weight calculation
-    W(:,k) = W(:,k-1) + 2*mu*en(k)*x;
-end
-% The output sequence of the filter when finding the optimal r If there is no yn return parameter, the following can be omitted
-yn = inf * ones(size(xn)); % inf means infinity
-for k = M:length(xn)
-    x = xn(k:-1:k-M+1);
-    yn(k) = W(:,end).'* x; % Get the output with the last best estimate
+% Reshape signal data for S/P conversion
+p_data = reshape(s_data, 32, Lfr);
+
+% IDFT to convert to time-domain
+p_td = ifft(p_data);
+
+% Cyclic prefix
+p_cyc = [p_td(end-4:end, :); p_td]; % Add cyclic prefix
+s_cyc = reshape(p_cyc, 37*Lfr, 1); % P/S conversion
+
+% Find the channel in frequency-domain
+hf = fft(channel, 32);
+
+% Generate channel output signal
+chsout = filter(channel, 1, s_cyc);
+
+% Add complex Gaussian noise
+noiseq = (randn(37*Lfr, 1) + 1i*randn(37*Lfr, 1));
+
+% Calculate average channel input power
+Psig = 10/32;
+
+% Define SNR range
+SNR = 0:30;
+
+% Define I/Q imbalance parameters
+alpha = chsout * 0.3;
+y_conj = conj(chsout);
+beta = y_conj * 0.7;
+y_bar = alpha + beta;
+
+% Initialize SEReq array
+SEReq = [];
+
+% Apply LMS algorithm for each SNR level
+for ii = 1:length(SNR)
+    
+    % Add noise to channel output signal
+    Asig = sqrt(Psig * 10^(-SNR(ii)/10)) * norm(channel);
+    y_t = chsout + Asig * noiseq;
+    
+    % Convert signal to parallel form for LMS equalization
+    n_para = reshape(y_bar, 37, Lfr);
+    n_disc = n_para(6:37, :);
+    nhat_para = fft(n_disc);
+    n_data = inv(diag(hf)) * nhat_para;
+    
+    % Calculate convergence factor for LMS
+    mu = 0.00018; % mu convergence factor (step size)
+    
+    % Define LMS filter parameters
+    M = 32; % Number of filter taps
+    itr = length(n_data); % Number of iterations
+    en = zeros(itr, 1); % Error sequence
+    W = zeros(M, itr); % Weighting parameter matrix
+    
+    % Iterative calculation for LMS filter
+    for k = M:itr
+        x = n_data(k:-1:k-M+1);
+        y = W(:, k-1).' * x;
+        en(k) = p_data(k) - y;
+        W(:, k) = W(:, k-1) + 2*mu*en(k)*x;
+    end
+    
+    % Get output sequence of the filter with the last best estimate
+    yn = inf * ones(size(n_data));
+    for k = M:length(n_data)
+        x = n_data(k:-1:k-M+1);
+        yn(k) = W(:, end).'
 end
 %%
 x_para=reshape(yn,37,Lfr); % S/P conversion
